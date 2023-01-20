7f59584021223ec93114a0fa7df494ff85a189c3
Kyle Bocinsky <bocinsky@gmail.com>
2023-01-19 19:19:53 -0700

    updated script and maps

--- fsa-lfp-eligibility.R
+++ fsa-lfp-eligibility.R
@@ -7,12 +7,6 @@ conus <-
   rmapshaper::ms_simplify() %>%
   sf::st_transform("EPSG:5070")
 
-# lfp_eligibility %>%
-#   dplyr::select(PASTURE_CODE, PASTURE_TYPE) %>%
-#   dplyr::distinct() %>%
-#   dplyr::arrange(PASTURE_CODE) %>%
-#   as.data.frame()
-
 lfp_eligibility <-
   unzip("foia/2023-FSA-00937-F Bocinsky.zip", list = TRUE) %$%
   Name %>%
@@ -66,12 +60,11 @@ lfp_eligibility <-
                 -FSA_CNTY_CODE,
                 -START,
                 -END
-                ) %>%
+  ) %>%
   dplyr::select(PROGRAM_YEAR, 
                 FSA_CODE, 
                 FSA_STATE, 
-                FSA_COUNTY_NAME, 
-                PASTURE_CODE, 
+                FSA_COUNTY_NAME,
                 PASTURE_TYPE, 
                 dplyr::everything()) %>%
   dplyr::mutate(FSA_CODE = ifelse(FSA_CODE == "12025", "12086", FSA_CODE), # "DADE" presumed to be Miami-Dade
@@ -81,9 +74,29 @@ lfp_eligibility <-
                 FSA_CODE = ifelse(FSA_CODE == "29193", "29186", FSA_CODE), # FSA mis-labeled Ste. Genevieve, MO in some years. Re-labeling.
                 FSA_CODE = ifelse(FSA_CODE == "46113", "46102", FSA_CODE), # FSA mis-labeled Oglala Lakota, SD (Formerly Shannon, SD) in some years. Re-labeling.
                 FSA_CODE = ifelse(FSA_CODE == "27112", "27111", FSA_CODE), # FSA mis-labeled Otter Tail, MN in some years. Re-labeling.
-                ) %>%
+  ) %>%
   dplyr::filter(FSA_CODE != "27138") %>% # This seems to have been an error. No other crops have the same start date
-  dplyr::mutate(FSA_CODE = stringr::str_pad(FSA_CODE, 5, pad = "0"))
+  dplyr::mutate(
+    FSA_CODE = stringr::str_pad(FSA_CODE, 5, pad = "0"),
+    PASTURE_TYPE = 
+      ifelse(PASTURE_TYPE == "SHRT SEASON SMALL GRAIN 1",
+             "SHORT SEASON SMALL GRAINS",
+             PASTURE_TYPE),
+    PASTURE_TYPE = 
+      ifelse(PASTURE_TYPE == "FULL SEASON IMPROVE MIXED",
+             "FULL SEASON IMPROVED — MIXED",
+             PASTURE_TYPE),
+    PASTURE_TYPE = 
+      ifelse(PASTURE_TYPE == "SHORT SSN SPRING SML GRN",
+             "SHORT SEASON SMALL GRAINS — SPRING",
+             PASTURE_TYPE),
+    PASTURE_TYPE = 
+      ifelse(PASTURE_TYPE == "SHRT SSN FALL_WTR SML GRN",
+             "SHORT SEASON SMALL GRAINS — FALL–WINTER",
+             PASTURE_TYPE),
+    PASTURE_TYPE = stringr::str_to_title(PASTURE_TYPE),
+    FSA_COUNTY_NAME = stringr::str_to_title(FSA_COUNTY_NAME)
+  )
 
 lfp_eligibility %>%
   dplyr::arrange(PROGRAM_YEAR, FSA_CODE, PASTURE_TYPE) %>%
@@ -101,38 +114,38 @@ lfp_eligibility_graphs <-
     graph = list(
       (
         dplyr::left_join(conus, data,
-                     by = c("GEOID" = "FSA_CODE")) %>%
-      ggplot2::ggplot() +
-      geom_sf(aes(fill = FACTOR),
-              col = "white") +
-      geom_sf(data = conus %>%
-                dplyr::group_by(STATEFP) %>%
-                dplyr::summarise(),
-              col = "white",
-              fill = NA,
-              linewidth = 0.5) +
-      scale_fill_manual(values = c("1" = "#FFFF54",
-                                   "2" = "#F3AE3D",
-                                   "3" =  "#6D4E16",
-                                   "4" = "#EA3323",
-                                   "5" =  "#7316A2"),
-                        drop = FALSE,
-                        name = paste0("Eligible County Payment Months\n", PROGRAM_YEAR, ", ", stringr::str_to_title(PASTURE_TYPE)),
-                        guide = guide_legend(direction = "horizontal",
-                                             title.position = "top"),
-                        na.value = "grey80") +
-      theme_void(base_size = 24) +
-      theme(legend.position = c(0.225,0.125),
-            # legend.key.width = unit(0.1, "npc"),
-            legend.title = element_text(size = 14),
-            legend.text = element_text(size = 12),
-            strip.text.x = element_text(margin = margin(b = 5)))
-    ) %T>%
-      ggsave(filename = paste0("maps/", PROGRAM_YEAR, "-", stringr::str_to_title(PASTURE_TYPE),".png"),
-             device = png,
-             width = 10,
-             height = 6.86,
-             bg = "white",
-             dpi = 600)
+                         by = c("GEOID" = "FSA_CODE")) %>%
+          ggplot2::ggplot() +
+          geom_sf(aes(fill = FACTOR),
+                  col = "white") +
+          geom_sf(data = conus %>%
+                    dplyr::group_by(STATEFP) %>%
+                    dplyr::summarise(),
+                  col = "white",
+                  fill = NA,
+                  linewidth = 0.5) +
+          scale_fill_manual(values = c("1" = "#FFFF54",
+                                       "2" = "#F3AE3D",
+                                       "3" =  "#6D4E16",
+                                       "4" = "#EA3323",
+                                       "5" =  "#7316A2"),
+                            drop = FALSE,
+                            name = paste0("Eligible County Payment Months\n", PROGRAM_YEAR, ", ", stringr::str_to_title(PASTURE_TYPE)),
+                            guide = guide_legend(direction = "horizontal",
+                                                 title.position = "top"),
+                            na.value = "grey80") +
+          theme_void(base_size = 24) +
+          theme(legend.position = c(0.225,0.125),
+                # legend.key.width = unit(0.1, "npc"),
+                legend.title = element_text(size = 14),
+                legend.text = element_text(size = 12),
+                strip.text.x = element_text(margin = margin(b = 5)))
+      ) %T>%
+        ggsave(filename = paste0("maps/", PROGRAM_YEAR, "-", stringr::str_to_title(PASTURE_TYPE),".png"),
+               device = png,
+               width = 10,
+               height = 6.86,
+               bg = "white",
+               dpi = 600)
     )
   )
